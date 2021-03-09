2c81554e82c2f30b2050f81af4fcb6f7f6351475
saebragani <51421692+saebragani@users.noreply.github.com>
2021-03-09 01:58:45 -0500

    launch portfolio

---
+++ myScript.js
@@ -0,0 +1,37 @@
+function openCity(cityName, elmnt, color) {
+// Hide all elements with class="tabcontent" by default */
+var i, tabcontent, tablinks;
+tabcontent = document.getElementsByClassName("tabcontent");
+for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+}
+
+// Remove the background color of all tablinks/buttons
+tablinks = document.getElementsByClassName("tablink");
+for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+}
+
+// Show the specific tab content
+document.getElementById(cityName).style.display = "block";
+
+// Add the specific color to the button used to open the tab content
+elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
+
+
+function printDiv(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
